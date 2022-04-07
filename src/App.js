91ac9ed30ed8c85febf82f86c3bb19bfcbd392cc
paul117@naver.com <paul117@naver.com>
2022-04-07 09:47:28 +0900

    02-2 깃허브에 앱 업로드

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({name}) {
+  return <h1>I like {name}</h1>;
+}
+
+const foodILike =[
+  {
+    name: 'Kimchi',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSI5zbitDmSr5pOFWiP3qnAhv04pAe-pi1gvA&usqp=CAU'
+  },
+  {
+    name: 'Samgyeopsal',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmPbFhWQxUlVOn3z0GXbprWSrlN-eTtxy-XA&usqp=CAU'
+  },
+  {
+    name:'Bibimbap',
+    image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk8TgzcEGvR9rAOG8unUUaLnKWwyzRgIl5ew&usqp=CAU'
+  },
+  {
+    name:'Doncasu',
+    image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVecIPObOrlhha3phjqGB4cH6yNVEh6_FuwY5tC_gWopcIUHVonh1vVkl6Ao-0fVukcs4&usqp=CAU'
+  },
+  {
+    name:'Kimbap',
+    image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZ7JhEx6mwZ86gmYiCGmLxce3_R31-3MOfg&usqp=CAU'
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      {foodILike.map(dish => <Food key={dish.name} name={dish.name} />)}
     </div>
   );
 }
+
 
 export default App;
